a2c8c9207e1c7356d7c8f144c37efbacecdf7a7a
putraadipradana <putraadi.pradana29@gmail.com>
2026-01-21 23:00:51 +0800

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <Link href="/" className="flex items-center space-x-3">
-          <IconInnerShadowTop className="size-10!" />
+          <IconInnerShadowTop className="size-10! text-teal-500 dark:text-teal-400" />
           <span className="font-semibold text-xl">Ordr</span>
         </Link>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
@@ -33,7 +33,7 @@ export default function Page() {
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <Link
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-primary px-5 text-background transition-colors hover:bg-primary/90 md:w-39.5"
             href="/dashboard"
             rel="noopener noreferrer"
           >
